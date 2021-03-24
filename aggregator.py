19742d4bdfeb9861c8384e7193b8449d0c10ddae
Will <will.firman@gmail.com>
2021-03-24 17:13:58 +0000

    Added checks for changes to the feeds.json file after the server started Updated reader.Reader with an update frequency to avoid checking RSS feeds every time a page is refreshed Replaced {% include %} blocks in home.html with the equivalent Flask route function Various extra documentation added

--- aggregator.py
+++ aggregator.py
@@ -3,46 +3,87 @@ from flask import Blueprint, render_template
 
 from reader import Reader
 
-with open('config/feeds.json') as f:
-    feed_settings = json.load(f)
 
-feed_dict = {}
-for feed_setting in feed_settings:
-    feed_reader = Reader(
-        identifier=feed_setting,
-        name=feed_settings[feed_setting]['name'],
-        site_url=feed_settings[feed_setting]['site_url'],
-        feed_url=feed_settings[feed_setting]['feed_url']
-    )
+# Globals setup:
+feed_settings = {}  # Dict to hold information from config file
+feed_dict = {}      # Dict to hold a reader object for each feed
 
-    feed_dict[feed_setting] = feed_reader
+
+def get_reader(feed):
+
+    if feed_settings.get(feed) is None:
+        # Specified feed is not found, check if it's been added to feeds.json
+        get_feed_settings()
+
+    if feed_dict.get(feed) is None:
+        # No reader exists for this feed, initialise one
+        print(f"initialising reader for {feed}")
+
+        try:
+            reader = Reader(
+                identifier=feed,
+                name=feed_settings[feed]['name'],
+                site_url=feed_settings[feed]['site_url'],
+                feed_url=feed_settings[feed]['feed_url']
+            )
+        except KeyError:
+            raise KeyError(f"{feed} not found, add it to feeds.json")
+
+        feed_dict[feed] = reader
+
+    return feed_dict[feed]
+
+
+def get_feed_settings():
+    global feed_settings
+    with open('config/feeds.json') as f:
+        feed_settings = json.load(f)
+
+
+# Initial setup
+get_feed_settings()
+
+# Create Reader instance for each feed in config file
+for feed in feed_settings:
+    get_reader(feed)
 
 aggregator_bp = Blueprint('routes', __name__, template_folder='templates')
 
 
 @aggregator_bp.route('/')
 def home():
+
+    # Check for any new feeds added to the config file since server started
+    get_feed_settings()
+
+    # Initialise readers for any new feeds
+    for feed in feed_settings:
+        get_reader(feed)
+
     item_count = len(feed_dict)
     items_per_row = 2
     row_count = item_count // items_per_row
     leftover_items = item_count % items_per_row
 
     return render_template('home.html',
-                           feeds_dict=feed_dict,
                            feeds_dict_keys=list(feed_dict.keys()),
                            row_count=row_count,
                            items_per_row=items_per_row,
-                           leftover_items=leftover_items
+                           leftover_items=leftover_items,
+                           show_feed=show_feed
                            )
 
 
 @aggregator_bp.route('/feed/<feed>/<int:num_entries>')
-def show_feed(feed, num_entries):
-    feed_reader = feed_dict[feed]
+def show_feed(feed, num_entries, being_included=False):
+
+    feed_reader = get_reader(feed)
     feed_reader.refresh_entries(num_entries)
 
-    return render_template('feed.html', reader=feed_reader,
-                           num_entries=num_entries)
+    return render_template('feed.html',
+                           reader=feed_reader,
+                           num_entries=num_entries,
+                           being_included=being_included)
 
 
 @aggregator_bp.route('/feeds')
--- reader.py
+++ reader.py
@@ -1,24 +1,42 @@
 import feedparser
+from datetime import datetime, timezone as tz, timedelta
 
 
 class Reader:
 
-    def __init__(self, identifier, name, site_url, feed_url, num_entries=6):
+    def __init__(self, identifier, name, site_url, feed_url, num_entries=6,
+                 update_frequency=5):
         self.identifier = identifier
         self.name = name
         self.site_url = site_url
         self.feed_url = feed_url
+        self.update_frequency = timedelta(minutes=update_frequency)
         self.feed_data = None
         self.entries = None
+        self.last_updated = None
 
         self.refresh_feed()
         self.refresh_entries(num_entries)
 
     def refresh_feed(self):
-        self.feed_data = feedparser.parse(self.feed_url)
+        """Refresh the RSS feed, if the required amount of time has elapsed
+        since the last update"""
+
+        # Check how long since this feed was updated. If this is the first
+        # time, handle TypeError
+        try:
+            since_update = datetime.now(tz.utc) - self.last_updated
+        except TypeError:
+            since_update = None
+
+        if since_update is None or since_update > self.update_frequency:
+            self.feed_data = feedparser.parse(self.feed_url)
+            self.last_updated = datetime.now(tz.utc)
 
     def refresh_entries(self, num_entries=None):
-        if self.feed_data is None:
-            self.refresh_feed()
+        """Get the requested number of entries from the RSS feed, refreshing it
+        from the source if necessary"""
+
+        self.refresh_feed()
 
         self.entries = self.feed_data.entries[:num_entries]
